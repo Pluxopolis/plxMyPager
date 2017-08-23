5205563448a73b94608ac29f8f9b25cd7b8f397f
Steffen Schultz <schulle4u@users.noreply.github.com>
2017-08-23 12:03:00 +0200

    Create de.php

---
+++ lang/de.php
@@ -0,0 +1,20 @@
+<?php
+
+$LANG = array(
+
+# config.php
+'L_PAGER_CHECK_LIB'				=> 'Anzuzeigende Einträge anhaken',
+'L_PAGER_NUM_PAGES'				=> 'Anzahl der Seiten',
+'L_PAGER_INDICATOR'				=> 'Indikator',
+'L_PAGER_SAVE'					=> 'Speichern',
+
+'L_PAGER_CSS_DESCRIPTION'		=> 'Die Anzeige kann im Stylesheet des Themes mittels der folgenden CSS-Klassen angepasst werden',
+'L_PAGER_CSS_ITEM'				=> 'Eintrag der Seitennavigation',
+'L_PAGER_CSS_FIRST'				=> 'Erste Seite',
+'L_PAGER_CSS_PREV'				=> 'Vorige Seite',
+'L_PAGER_CSS_CURRENT'			=> 'Aktuelle seite',
+'L_PAGER_CSS_NEXT'				=> 'Nächste Seite',
+'L_PAGER_CSS_LAST'				=> 'Letzte Seite',
+
+);
+?>
